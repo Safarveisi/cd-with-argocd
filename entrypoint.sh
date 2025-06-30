6e6a81eabbb9765eaeb0cdfde195cbf58f33fd91
Safarveisi <sajad.safarveisi@gmail.com>
2025-06-30 15:56:19 +0200

    Added sealed secret using kubeseal and updated run.sh and manifests

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,6 @@
 
 echo "${STARTUP_MESSAGE:-Hello, World!}"
 python app/main.py
+echo "${PASS1:-You} ${PASS2:-Made it!}"
 echo "🛌 Sleeping for $SLEEP_DURATION seconds..."
 sleep "$SLEEP_DURATION"
--- run.sh
+++ run.sh
@@ -4,6 +4,23 @@ STARTING_PATH=$(git rev-parse --show-toplevel)
 IDENTIFIER_COMMENT="# LATEST_IMAGE_TAG"
 
 
+function install:sealed_secrets_controller {
+    helm repo add sealed-secrets https://bitnami-labs.github.io/sealed-secrets
+    helm repo update sealed-secrets
+    helm install sealed-secrets \
+        -n kube-system --set-string fullnameOverride=sealed-secrets-controller \
+        sealed-secrets/sealed-secrets
+}
+
+# Make sure kubeseal is installed
+function create:sealed_secrets {
+    if [[ $# -ne 2 ]]; then
+        echo "Usage: create:sealed_secrets <input-file> <output-file>"
+        return 1
+    fi
+    kubeseal --format yaml -f "$1" > "$2"
+}
+
 function get_project_version {
     echo "v$(cat pyproject.toml | grep 'version =' | sed -E 's/version = //' | tr -d '"=')"
 }
